e169f39529ec149ff0927209092968c77c9100b3
inexperiencedCoder32 <112755149+inexperiencedCoder32@users.noreply.github.com>
2022-09-21 14:37:03 +0400

    Add files via upload

---
+++ Day 2/lesson2.py
@@ -0,0 +1,8 @@
+name = "Oto"   #string
+surname = "adamashvili"
+age = 16     #integer
+height = 185   #float 
+is_12thGrader = False  #Boolean
+
+print("My name is " + name + " i'm " + str(age) + " years old" + " i am " + str(height) + " centimeters in height.")
+print("Is a 12th grader: " + str(is_12thGrader))
